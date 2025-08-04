b11f35276e8b09f8013a789c7f524727f9248595
RISHABH SHAH <rnshah1997@gmail.com>
2025-08-04 19:04:02 -0400

    final Upload

--- app/page.js
+++ app/page.js
@@ -2,8 +2,20 @@ import ClientDemo from "@/components/clientDemo";
 import DataFetchingDemo from "@/components/DataFetchingDemo";
 import RSCDemo from "@/components/RSCdemo";
 import ServerActionsDemo from "@/components/ServerActions";
+import UsePromiseDemo from "@/components/UsePromiseDemo";
+import { Suspense, use } from "react";
+import { readFile } from "node:fs/promises";
+import ErrorBoundary from "@/components/ErrorBoundry";
 
-export default function Home() {
+export default async function Home() {
+  const fetchUserPromise = new Promise((resolve, reject) =>
+    setTimeout(async () => {
+      const dataDemo = await readFile("dummy-db.json", "utf-8");
+      const users = JSON.parse(dataDemo);
+      resolve(users);
+    }, 2000)
+  );
+
   return (
     <main>
       <ClientDemo>
@@ -11,6 +23,12 @@ export default function Home() {
         <DataFetchingDemo />
         <ServerActionsDemo />
       </ClientDemo>
+
+      <ErrorBoundary fallback={<p>something went wrong..</p>}>
+        <Suspense fallback={<p>loading...</p>}>
+          <UsePromiseDemo usersPromise={fetchUserPromise} />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
--- components/UsePromiseDemo.js
+++ components/UsePromiseDemo.js
@@ -1,4 +1,7 @@
-export default function UsePromiseDemo({ users }) {
+"use client";
+import { use } from "react";
+export default function UsePromiseDemo({ usersPromise }) {
+  const users = use(usersPromise);
   return (
     <div className="rsc">
       <h2>RSC with Data Fetching</h2>
@@ -15,3 +18,8 @@ export default function UsePromiseDemo({ users }) {
     </div>
   );
 }
+
+// It is used to access promises wihtout using async await in the client side components.
+// It can also be used for getting access to Context.
+// It Works together with suspense to handle data fetching and loading fallbacks.
+// this use hook only works with suspense feature or it works with the specila promise that is used by react server components.
